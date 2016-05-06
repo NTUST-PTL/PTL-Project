735e2da6a4319152d4ea5f3626e096de98f4f345
F1shift Lee <F1shift Lee>
2016-05-06 14:32:29 +0800

    2016-5-6 Improved SolidWorks launch process.

--- SolidWorks/SolidWorksAppAdapter.cs
+++ SolidWorks/SolidWorksAppAdapter.cs
@@ -10,8 +10,10 @@ namespace PTL.SolidWorks
 {
     public class SolidWorksAppAdapter
     {
-        public String SolidWorksPath { get { return FindSolidWorksPath(); } }
         public ISldWorks SWApp { get; protected set; }
+        public String SWPath { get; protected set; }
+        public String SWVersion { get; protected set; }
+
         public IModelDoc2 ActiveDoc
         {
             get
@@ -28,32 +30,36 @@ namespace PTL.SolidWorks
         protected virtual ISldWorks CreatSolidWorksApplication()
         {
             ISldWorks swApp = null;
-            String SWPath = FindSolidWorksPath();
-            if (SWPath != null)
+            IEnumerable<KeyValuePair<String, String>> SWVersionAndPaths = FindSolidWorksPath();
+            foreach (var VersionAndPaths in SWVersionAndPaths)
             {
                 //截入DLL檔
-                System.Reflection.Assembly sldworks_dll = System.Reflection.Assembly.LoadFrom(SWPath + "SolidWorks.Interop.sldworks.dll");
+                if (System.IO.File.Exists(VersionAndPaths.Value + "SolidWorks.Interop.sldworks.dll"))
+                {
+                    System.Reflection.Assembly sldworks_dll = System.Reflection.Assembly.LoadFrom(VersionAndPaths.Value + "SolidWorks.Interop.sldworks.dll");
 
-                //開啟SolidWorks
-                swApp = (ISldWorks)sldworks_dll.CreateInstance("SolidWorks.Interop.sldworks.SldWorksClass");
-                swApp.Visible = true;
+                    //開啟SolidWorks
+                    swApp = (ISldWorks)sldworks_dll.CreateInstance("SolidWorks.Interop.sldworks.SldWorksClass");
+                    swApp.Visible = true;
+
+                    SWVersion = VersionAndPaths.Key;
+                    SWPath = VersionAndPaths.Value;
+
+                    return swApp;
+                }
             }
-            else
-            {
-                System.Windows.MessageBox.Show("找不到已安裝的SolidWorks");
-            }
-            return swApp;
+
+            throw new TypeAccessException("找不到已安裝的SolidWorks!");
+            return null;
         }
-        protected virtual String FindSolidWorksPath()
+        protected virtual IEnumerable<KeyValuePair<String, String>> FindSolidWorksPath()
         {
-            String Path = null;
-
             //從註冊機尋找安裝路徑
             Microsoft.Win32.RegistryKey LocalMachineRegistryKey32 = Microsoft.Win32.RegistryKey.OpenBaseKey(Microsoft.Win32.RegistryHive.LocalMachine, Microsoft.Win32.RegistryView.Registry32);
             Microsoft.Win32.RegistryKey LocalMachineRegistryKey64 = Microsoft.Win32.RegistryKey.OpenBaseKey(Microsoft.Win32.RegistryHive.LocalMachine, Microsoft.Win32.RegistryView.Registry64);
 
             //從2012年檢查到2020年，尋找已安裝的SolidWorks的安裝目錄
-            for (int i = 2012; i < 2020; i++)
+            for (int i = 2012; i < 2050; i++)
             {
                 Microsoft.Win32.RegistryKey SolidWorksSetupRegistryKey;
 
@@ -61,22 +67,20 @@ namespace PTL.SolidWorks
                 SolidWorksSetupRegistryKey = LocalMachineRegistryKey64.OpenSubKey(@"SOFTWARE\SolidWorks\SolidWorks " + i.ToString() + @"\Setup");
                 if (SolidWorksSetupRegistryKey != null)
                 {
-                    Path = (String)SolidWorksSetupRegistryKey.GetValue("SolidWorks Folder");
+                    String Path = (String)SolidWorksSetupRegistryKey.GetValue("SolidWorks Folder");
                     if (Path != null)
-                        break;
+                        yield return new KeyValuePair<string, string>("SolidWorks " + i.ToString(), Path);
                 }
 
                 //檢查是否安裝32位元版本的SolidWorks
                 SolidWorksSetupRegistryKey = LocalMachineRegistryKey32.OpenSubKey(@"SOFTWARE\SolidWorks\SolidWorks " + i.ToString() + @"\Setup");
                 if (SolidWorksSetupRegistryKey != null)
                 {
-                    Path = (String)SolidWorksSetupRegistryKey.GetValue("SolidWorks Folder");
+                    String Path = (String)SolidWorksSetupRegistryKey.GetValue("SolidWorks Folder");
                     if (Path != null)
-                        break;
+                        yield return new KeyValuePair<string, string>("SolidWorks " + i.ToString(), Path);
                 }
             }
-
-            return Path;
         }
 
         public virtual IModelDoc2 CreatePart()
@@ -84,7 +88,13 @@ namespace PTL.SolidWorks
             IModelDoc2 newModDoc2 = null;
             if (this.SWApp != null)
             {
-                string modDocTemplate = this.SWApp.GetUserPreferenceStringValue(8);
+                string modDocTemplate = System.IO.File.Exists("C:\\ProgramData\\SolidWorks\\" + SWVersion + "\\templates\\零件.prtdot")
+                    ? "C:\\ProgramData\\SolidWorks\\" + SWVersion + "\\templates\\零件.prtdot"
+                    : this.SWApp.GetUserPreferenceStringValue(8);
+                if (!System.IO.File.Exists(modDocTemplate))
+                {
+                    throw new TypeAccessException("找不到SolidWorks零件樣板檔案!");
+                }
                 newModDoc2 = this.SWApp.NewDocument(modDocTemplate, 0, 0.0, 0.0);
             }
             return newModDoc2;
