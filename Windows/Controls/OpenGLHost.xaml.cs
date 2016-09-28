b23284dcda98689bd5c46dbfa6214bb6f37c4312
F1shift <lif1shift@gmail.com>
2016-09-28 16:34:56 +0800

    2016-9-28

--- Windows/Controls/OpenGLHost.xaml.cs
+++ Windows/Controls/OpenGLHost.xaml.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.IO;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -73,6 +74,21 @@ namespace PTL.Windows.Controls
             get { return (Dimension)GetValue(DimensionProperty); }
             set { SetValue(DimensionProperty, value); }
         }
+        public bool PlotGrid
+        {
+            get { return (bool)GetValue(PlotGridProperty); }
+            set { SetValue(PlotGridProperty, value); }
+        }
+        public bool PlotGraduation
+        {
+            get { return (bool)GetValue(PlotGraduationProperty); }
+            set { SetValue(PlotGraduationProperty, value); }
+        }
+        public double GraduationHeight
+        {
+            get { return (double)GetValue(GraduationHeightProperty); }
+            set { SetValue(GraduationHeightProperty, value); }
+        }
 
         // Using a DependencyProperty as the backing store for GridLineColor1.  This enables animation, styling, binding, etc...
         public static readonly DependencyProperty GridColor1Property =
@@ -101,6 +117,15 @@ namespace PTL.Windows.Controls
         // Using a DependencyProperty as the backing store for Dimension.  This enables animation, styling, binding, etc...
         public static readonly DependencyProperty DimensionProperty =
             DependencyProperty.Register("Dimension", typeof(Dimension), typeof(OpenGLHost), new PropertyMetadata(Dimension.ThreeDimension, OnAdaptedWindowPropertyChangedCallback));
+        // Using a DependencyProperty as the backing store for Dimension.  This enables animation, styling, binding, etc...
+        public static readonly DependencyProperty PlotGridProperty =
+            DependencyProperty.Register("PlotGrid", typeof(bool), typeof(OpenGLHost), new PropertyMetadata(true, OnAdaptedViewPropertyChangedCallback));
+        // Using a DependencyProperty as the backing store for Dimension.  This enables animation, styling, binding, etc...
+        public static readonly DependencyProperty PlotGraduationProperty =
+            DependencyProperty.Register("PlotGraduation", typeof(bool), typeof(OpenGLHost), new PropertyMetadata(true, OnAdaptedViewPropertyChangedCallback));
+        // Using a DependencyProperty as the backing store for FrameInterval.  This enables animation, styling, binding, etc...
+        public static readonly DependencyProperty GraduationHeightProperty =
+            DependencyProperty.Register("GraduationHeight", typeof(double), typeof(OpenGLHost), new PropertyMetadata(20.0, OnAdaptedViewPropertyChangedCallback));
 
 
 
@@ -109,6 +134,7 @@ namespace PTL.Windows.Controls
             DataContextProperty.OverrideMetadata(typeof(OpenGLHost), new FrameworkPropertyMetadata(OnDataContextChangedCallback));
             BackgroundProperty.OverrideMetadata(typeof(OpenGLHost), new FrameworkPropertyMetadata( System.Windows.Media.Brushes.DimGray, OnBackgroundChangedCallback));
             ForegroundProperty.OverrideMetadata(typeof(OpenGLHost), new FrameworkPropertyMetadata( System.Windows.Media.Brushes.LightGray, OnForegroundChangedCallback));
+            FontSizeProperty.OverrideMetadata(typeof(OpenGLHost), new FrameworkPropertyMetadata(20.0, OnFontSizeChangedCallback));
         }
 
         public OpenGLHost()
@@ -145,6 +171,11 @@ namespace PTL.Windows.Controls
             (o as OpenGLHost).OpenGLViewer.View.GridColor2 = (System.Drawing.Color)converter.Convert(e.NewValue, typeof(System.Drawing.Color), null, null);
         }
 
+        private static void OnFontSizeChangedCallback(DependencyObject o, DependencyPropertyChangedEventArgs e)
+        {
+            (o as OpenGLHost).GraduationHeight = (double)e.NewValue;
+        }
+
         private static void OnAdaptedViewPropertyChangedCallback(DependencyObject o, DependencyPropertyChangedEventArgs e)
         {
             var view = (o as OpenGLHost).OpenGLViewer.View;
@@ -155,6 +186,62 @@ namespace PTL.Windows.Controls
         {
             var viewer = (o as OpenGLHost).OpenGLViewer;
             viewer.GetType().GetProperty(e.Property.Name).SetValue(viewer, e.NewValue);
+        }
+
+        public void CaptureStaticImage()
+        {
+            //double width = this.ActualWidth;
+            //double height = this.ActualHeight;
+            //RenderTargetBitmap bmpCopied = new RenderTargetBitmap((int)Math.Round(width), (int)Math.Round(height), 96, 96, PixelFormats.Default);
+            //DrawingVisual dv = new DrawingVisual();
+            //using (DrawingContext dc = dv.RenderOpen())
+            //{
+            //    VisualBrush vb = new VisualBrush(this);
+            //    dc.DrawRectangle(vb, null, new Rect(new System.Windows.Point(), new System.Windows.Size(width, height)));
+            //}
+            //bmpCopied.Render(dv);
+
+            //var bitmapImage = new BitmapImage();
+            //var bitmapEncoder = new PngBitmapEncoder();
+            //bitmapEncoder.Frames.Add(BitmapFrame.Create(bmpCopied));
+
+            //using (var stream = new MemoryStream())
+            //{
+            //    bitmapEncoder.Save(stream);
+            //    stream.Seek(0, SeekOrigin.Begin);
+
+            //    bitmapImage.BeginInit();
+            //    bitmapImage.CacheOption = BitmapCacheOption.OnLoad;
+            //    bitmapImage.StreamSource = stream;
+            //    bitmapImage.EndInit();
+            //}
+
+            var bmp = new System.Drawing.Bitmap(this.OpenGLViewer.Width, this.OpenGLViewer.Height);
+            OpenGLViewer.DrawToBitmap(bmp, new System.Drawing.Rectangle(0, 0, bmp.Width, bmp.Height));
+
+            MemoryStream ms = new MemoryStream();
+            bmp.Save(ms, System.Drawing.Imaging.ImageFormat.Bmp);
+            BitmapImage image = new BitmapImage();
+            image.BeginInit();
+            ms.Seek(0, SeekOrigin.Begin);
+            image.StreamSource = ms;
+            image.EndInit();
+
+            this.ReplaceImage.Source = image;
+        }
+
+        public void ShowStaticImage()
+        {
+            this.ReplaceImage.Visibility = System.Windows.Visibility.Visible;
+            this.WindowsFormsHost.Visibility = System.Windows.Visibility.Hidden;
+            this.TestUI.Visibility = System.Windows.Visibility.Hidden;
+        }
+
+        public void HideStaticImage()
+        {
+            this.WindowsFormsHost.Visibility = System.Windows.Visibility.Visible;
+            this.TestUI.Visibility = System.Windows.Visibility.Visible;
+            this.ReplaceImage.Visibility = System.Windows.Visibility.Hidden;
         }
     }
 }
